1e6ae73f2925028bcaed06ad3989fd0a49fe9847
Max Rangel <lik4ever@hotmail.com>
2020-12-06 19:18:57 -0600

    completed day 1 challenge

--- day-1/app.js
+++ day-1/app.js
@@ -1,7 +1,19 @@
-// Import data
+// Import data (number[])
+const data = require('./data.js');
 
 // Loop the data to find numbers that sum 2020
+let result = 0;
 
-// Multiply the numbers and store their result
+data.forEach(numA => {
+  data.forEach(numB => {
+    data.forEach(numC => {
+      // Multiply the numbers if sum of numA and numB is 2020
+      if (numA + numB + numC === 2020) {
+        result = numA * numB * numC;
+      }
+    });
+  });
+});
 
 // Once the loop is done, print the final result
+console.log(result);
